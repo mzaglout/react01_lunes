77209f4916e3591dab76c9bf818c2663a9020af1
lgutierr62 <lgutierr62@cuc.edu.co>
2024-09-16 09:40:23 -0500

    proyecto react 01

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miLogo from "./assets/img/logoReact.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Logo de React" />
+        <h1>¡¡¡Hola Mundo!!!</h1>
+        <p>Este es mi primer proyecto en React...</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
